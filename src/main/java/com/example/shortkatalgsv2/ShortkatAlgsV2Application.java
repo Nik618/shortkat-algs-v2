59b87c0a62522fcb9f17e052099283fd622cb8f3
Nik618 <trinax01@gmail.com>
2024-09-14 13:03:35 +0300

    init commit

---
+++ src/main/java/com/example/shortkatalgsv2/ShortkatAlgsV2Application.java
@@ -0,0 +1,13 @@
+package com.example.shortkatalgsv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortkatAlgsV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShortkatAlgsV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shortkatalgsv2/ShortkatAlgsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shortkatalgsv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortkatAlgsV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
