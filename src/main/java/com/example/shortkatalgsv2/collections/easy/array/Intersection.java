ef66d6f87d6b8347b9b859041bcd63f23d23d565
Nik618 <trinax01@gmail.com>
2024-09-15 14:51:25 +0300

    work 15.09

--- src/main/java/com/example/shortkatalgsv2/collections/easy/array/Intersection.java
+++ src/main/java/com/example/shortkatalgsv2/collections/easy/array/Intersection.java
@@ -2,7 +2,6 @@ package com.example.shortkatalgsv2.collections.easy.array;
 
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +9,8 @@ import java.util.List;
 @Service
 public class Intersection {
 
+    // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
+    // O(n^2)
 
     @PostConstruct
     public void start() {
@@ -18,9 +19,6 @@ public class Intersection {
 
     @PostConstruct
     public void intersection(Integer[] nums1, Integer[] nums2) {
-
-        // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
-        // O(n^2)
 
         List<Integer> arrayList1 = Arrays.stream(nums1).toList();
         List<Integer> arrayList2 = new ArrayList<>(Arrays.stream(nums2).toList());
