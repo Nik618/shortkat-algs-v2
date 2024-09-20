f84cb33b0e72b8f3a58ae8378f17c94cde7abfca
Nik618 <trinax01@gmail.com>
2024-09-20 22:07:00 +0300

    work 20.09

--- src/main/java/com/example/shortkatalgsv2/ReverseString2.java
+++ src/main/java/com/example/shortkatalgsv2/ReverseString2.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class ReverseString2 {
 
     // https://leetcode.com/problems/reverse-string-ii/description/
-    // O(n)
+    // O(n * (n/2))
 
     @PostConstruct
     private void start() {
@@ -15,19 +15,19 @@ public class ReverseString2 {
         process(new char[] {'s', 'n', 'o', 'w', 'e', 's', 't'}, 2);
     }
 
-    public void process(char[] string, int n) {
+    public void process(char[] string, int k) {
         int length = string.length;
         boolean flag = true;
         for (int i = 0; i <= length; i++) {
-            if ((i + 1) % n == 0 && i != 0 && flag) {
-                for (int j = i - n; j < (i + (i - n))/2; j++) {
+            if ((i + 1) % k == 0 && i != 0 && flag) {
+                for (int j = i - k; j < (i + (i - k))/2; j++) {
                     char temp = string[i];
                     string[i] = string[j + 1];
                     string[j + 1] = temp;
                 }
                 flag = false;
             } else {
-                if ((i + 1) % n == 0 && i != 0 && !flag) {
+                if ((i + 1) % k == 0 && i != 0 && !flag) {
                     flag = true;
                 }
             }
